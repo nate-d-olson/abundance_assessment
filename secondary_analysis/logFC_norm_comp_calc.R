519c7acf4a9c5eb700897814dffefdc2bc57c337
Nate Olson <nolson@nist.gov>
2019-09-19 22:16:24 -0400

    WIP:logFC norm assessment

---
+++ secondary_analysis/logFC_norm_comp_calc.R
@@ -0,0 +1,73 @@
+library(here)
+require(tidyverse)
+require(metagenomeSeq)
+mrexp_obj <- here("data","mothur_mrexp.rds") %>% readRDS()
+norm_count_mats <- here("data","norm_count_mats.RDS") %>% readRDS()
+
+make_titration_comp_df <- function(mrexp_obj){
+    pdat <- pData(mrexp_obj)  %>% 
+        mutate(t_fctr = factor(t_fctr, level = c(0:5,10,15,20)))
+    t1_pdat <- pdat %>% dplyr::select(biosample_id, id, t_fctr) %>% 
+        dplyr::rename(T1 = t_fctr, T1_id = id) 
+    t2_pdat <- pdat %>% dplyr::select(biosample_id, id, t_fctr) %>% 
+        dplyr::rename(T2 = t_fctr, T2_id = id) 
+    
+    ## Generate titration comparison sets and reduce redundancy
+    left_join(t1_pdat, t2_pdat) %>% 
+        filter(as.numeric(T1) < as.numeric(T2)) %>% 
+        group_by(biosample_id, T1, T2) %>% 
+        summarise(sam_names = c(T1_id,T2_id) %>% unique() %>% list(),
+                  n_sams = c(T1_id,T2_id) %>% unique() %>% length())
+}
+
+make_titration_comp_subset_df <- function(norm_mat, titration_comp_df, mrexp){
+    
+    ## Subsetting mrexp for titrations
+    subset_mat <- function(sam_names){
+        sub_mat <- norm_mat[,which(colnames(norm_mat) %in% sam_names)]
+        ## Remove features with no counts for all samples 
+        sub_mat <- sub_mat[rowSums(sub_mat) > 0,]
+    }
+    
+    get_design_mat <- function(sam_names){
+        mrexp_obj[,which(colnames(mrexp_obj) %in% sam_names)] %>% 
+            pData() %>% model.matrix(~t_fctr, data = .)
+    }
+    
+    titration_comp_df %>% 
+        ## Dataframe with list of subsetted mrexp
+        mutate(norm_mat_sub = map(sam_names, subset_mat),
+               design_mat = map(sam_names, get_design_mat))
+}
+
+calc_logFC <- function(subset_mat, design_mat){
+    require(limma)
+    fit <- lmFit(object = subset_mat,design = design_mat)
+    fit <- eBayes(fit)
+    
+    ## logFC and test results
+    topTable(fit,number = Inf) %>% 
+        rownames_to_column(var = "feature_id")
+}
+
+calc_titration_comp_logFC <- function(norm_count_mat, titation_comp_df){
+    titration_comp_df <- make_titration_comp_subset_df(norm_count_mat, titration_comp_df)  
+    
+    titration_comp_logFC_df <- titration_comp_df %>% 
+        mutate(logFC_df = map2(norm_mat_sub, design_mat, calc_logFC))
+}
+
+## Calculate logFC for titration comparisons for multiple normalization methods 
+titration_comp_df <- make_titration_comp_df(mrexp_obj)
+
+norm_logFC <- norm_count_mats %>% map(calc_titration_comp_logFC, titration_comp_df)
+saveRDS(norm_logFC, here("data","norm_logFC.RDS"))
+
+## Generate tidy data frame with logFC results
+norm_logFC_df <- norm_logFC %>% 
+    bind_rows(.id = "norm_method") %>%
+    select(biosample_id, norm_method, T1, T2, logFC_df) %>%
+    mutate(pipe = "mothur") %>% 
+    unnest()
+
+saveRDS(norm_logFC_df, here("data","norm_logFC_df.RDS"))
---
+++ secondary_analysis/rel_abu_norm_comp_calc.R
@@ -0,0 +1,80 @@
+library(here)
+library(metagenomeSeq)
+library(tidyverse)
+
+mrexp_obj <- readRDS(here("data","mothur_mrexp.rds"))
+
+normalize_counts <- function(method, mrexp){
+    require(matrixStats)
+    ## Extract count matrix from MRexperiment
+    count_mat <- mrexp@assayData$counts 
+    
+    ## extract normalizaed counts
+    if (method == "RAW") {
+        ## Raw counts no normalization applied
+        norm_factors = 1
+    } else if (method == "UQ") {
+        ## Upper quartile normalization
+        count_mat[count_mat == 0] <- NA
+        norm_factors <- colQuantiles(count_mat, p = 0.75 ,na.rm = TRUE)
+    } else if (method == "CSS") {
+        ## Cumulative sum scaling Paulson et al. 2013
+        norm_factors <- metagenomeSeq::calcNormFactors(count_mat, p = 0.75)
+        norm_factors <- norm_factors$normFactors
+    } else if ( method == "TSS") {
+        ## Total sum scaling 
+        norm_factors <- colSums(count_mat)
+    } else if (method %in% c("RLE","TMM")) {
+        ## EdgeR RLE and TMM normalization methods
+        norm_factors <- edgeR::calcNormFactors(count_mat, method = method) 
+        norm_factors <- norm_factors * colSums(count_mat)
+    } else {
+        warning("Normalization method not defined")
+    }
+    
+    ## Normalizing counts
+    sweep(count_mat, 2, norm_factors,'/')
+    
+}
+
+tidy_norm_counts <- function(norm_mat, mrexp){
+    ## Tidy counts
+    count_df <- norm_mat %>% 
+        as.data.frame() %>% 
+        rownames_to_column(var = "feature_id") %>% 
+        gather("id", "count", -feature_id)
+    
+    ## Adding sample data
+    count_df <- pData(mrexp) %>% 
+        select(biosample_id, id, t_fctr) %>% 
+        right_join(count_df)
+    
+    ## Summarizing count values across PCR replicates 
+    count_df %>% 
+        group_by(biosample_id, feature_id, t_fctr) %>% 
+        summarise(mean_count = mean(count),
+                  var_count = var(count),
+                  sd_count = sd(count))
+}
+
+## Normalizing counts using different methods/ normlization factors
+norm_methods <- list(RAW = "RAW", ## No normalization
+                     RLE = "RLE", ## EdgeR - relative log expression
+                     TMM = "TMM", ## EdgeR - weighted trim mean of M-values
+                     UQ = "UQ",   ## EdgeR - upperqurtile
+                     CSS = "CSS", ## metagenomeSeq - cumulative sum scaling, with p = 0.75
+                     TSS = "TSS") ## Total sum scaling (proportions)
+
+norm_count_mats <- norm_methods %>% 
+    map(normalize_counts, mrexp = mrexp_obj)
+
+saveRDS(norm_count_mats, here("data","norm_count_mats.RDS"))
+
+## Summarizing across PCR replicates and converting to a tidy data frame
+norm_count_df <- norm_count_mats %>% 
+    map(tidy_norm_counts, mrexp = mrexp_obj) %>% 
+    map_df(bind_rows, .id = "norm_method") %>%
+    mutate(pipe = "mothur")
+
+
+saveRDS(norm_count_df, here("data","norm_count_df.RDS"))
